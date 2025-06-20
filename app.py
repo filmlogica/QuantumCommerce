91e7a8deee5c10c690e47f441eaeae196624be45
filmlogica <filmlogica@gmail.com>
2025-06-20 08:33:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+import asyncio
+from background.generator import run_ai_product_generator
 
+app = FastAPI()
+
+@app.on_event("startup")
+async def load_quantum_engine():
+    asyncio.create_task(run_ai_product_generator())
